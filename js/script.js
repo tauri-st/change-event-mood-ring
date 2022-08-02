dbac77945de1f5e8dc1169049fddfd314338ef49
Tauri StClaire <104536950+tauri-st@users.noreply.github.com>
2022-08-01 18:07:50 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var body = document.querySelector("body");
+var dropDown = document.querySelector("#moods");
+
+dropDown.addEventListener("change", function (e) {
+  var mood = e.target.value;
+  if (mood === "happy") {
+    body.classList.remove("sad");
+    body.classList.remove("passionate");
+    body.classList.add("happy");
+  } else if (mood === "sad") {
+    body.classList.remove("happy");
+    body.classList.remove("passionate");
+    body.classList.add("sad");
+  }
+  if (mood === "passionate") {
+    body.classList.remove("sad");
+    body.classList.remove("happy");
+    body.classList.add("passionate");
+  }
+});
